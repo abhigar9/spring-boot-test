01d6594bb54609303d02e65e7c1b12f1240aca5e
abhishek garg <abhigar99@gmail.com>
2022-07-15 12:37:13 +0530

    Added first Rest Endpoint

--- spring-boot-test/src/main/java/com/abhi/jenkins/springboottest/SpringBootTestApplication.java
+++ spring-boot-test/src/main/java/com/abhi/jenkins/springboottest/SpringBootTestApplication.java
@@ -1,13 +1,27 @@
 package com.abhi.jenkins.springboottest;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class SpringBootTestApplication {
+@RestController
+public class SpringDockerApplication {
 
+	private final static Logger LOGGER = LoggerFactory.getLogger(SpringDockerApplication.class);
+	
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootTestApplication.class, args);
+		SpringApplication.run(SpringDockerApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	private String welcome() {
+		LOGGER.info("\n\n\n\nWelcome to Spring Github jenkins Docker App !!! \n\n");
+		return "Welcome to Spring Github jenkins Docker App !!! ";
 	}
 
 }
